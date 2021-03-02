6ea87cbedc0123bfbd1aa9d310aaa192b4a2239c
efloresc-dev <72379681+efloresc-dev@users.noreply.github.com>
2021-03-02 09:42:15 -0700

    Add files via upload
    Added the factorial function

--- GitDemo.java
+++ GitDemo.java
@@ -27,6 +27,14 @@ public class GitDemo {
 		return 0;
 	}
 	
-
+	/*****
+	 * This function should find the factorial of the 'num'
+	 *****/
+	public double findFactorial(double num) 
+	{ 
+	    if (num == 0) 
+	        return 1; 
+	    return num * findFactorial(num - 1); 
+	} 
 	
 }
